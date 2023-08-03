e4cfb0e2bf9505d283f0408365dad7afeb508b64
QueSchoenenberger <96289100+QueSchoenenberger@users.noreply.github.com>
2023-08-03 21:23:44 +0200

    get enhanced metadata from extract_metadata script

--- algorithms/cosine-similarity_algorithm.py
+++ algorithms/cosine-similarity_algorithm.py
@@ -5,59 +5,30 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from itertools import combinations
 
+from extract_metadata import get_metadata
+
 
 def convert_bytes_to_string(obj):
     if isinstance(obj, bytes):
         return obj.decode('utf-8')
-    raise TypeError
-
-
-def get_database_metadata(host_parameter, user_parameter, password_parameter, database_parameter):
-    try:
-        connection = mysql.connector.connect(host=host_parameter, user=user_parameter, password=password_parameter,
-                                             database=database_parameter)
-        cursor = connection.cursor()
-
-        cursor.execute("SHOW TABLES")
-        tables = cursor.fetchall()
-
-        metadata = {}
-
-        for table in tables:
-            table_name = table[0]
-            table_metadata = {}
-
-            cursor.execute(f"DESCRIBE {table_name}")
-            columns = cursor.fetchall()
-            column_names = [column[0] for column in cursor.description]
-
-            for column_info in columns:
-                column_info_dict = dict(zip(column_names, column_info))
-                column_name = column_info_dict['Field']
-                column_type = column_info_dict['Type']
-
-                table_metadata[column_name] = column_type
-
-            metadata[table_name] = table_metadata
-
-        cursor.close()
-        connection.close()
-
-        return metadata
-
-    except Exception as e:
-        print(f"Error: {str(e)}")
-        return None
+    return obj
 
 
 def convert_metadata_to_json(metadata):
     return json.dumps(metadata, indent=4, default=convert_bytes_to_string)
 
 
-def find_person_columns(metadata, similarity_threshold=0.9):
+def find_person_columns(metadata, relations, similarity_threshold=0.9):
     column_names = []
     for table_name, table_metadata in metadata.items():
-        column_names.extend(table_metadata.keys())
+        for column_name, column_data in table_metadata.items():
+            if not column_data['pk']:  # Exclude primary keys from similarity analysis
+                column_names.append(column_name)
+
+    # Add related column names to the list
+    for table_name, table_relations in relations.items():
+        for _, relation_data in table_relations.items():
+            column_names.append(relation_data['column'])
 
     vectorizer = TfidfVectorizer()
     X = vectorizer.fit_transform(column_names)
@@ -93,7 +64,6 @@ def find_person_columns(metadata, similarity_threshold=0.9):
     return cluster_results
 
 
-
 if __name__ == "__main__":
 
     # Shared
@@ -104,29 +74,30 @@ if __name__ == "__main__":
     # Database 1
     database1 = "classicmodels"
 
-    metadata1 = get_database_metadata(host, user, password, database1)
+    metadata1, relations1  = get_metadata(host, user, password, database1)
 
     # Database 2
     database2 = "sakila"
 
-    metadata2 = get_database_metadata(host, user, password, database2)
+    metadata2, relations2 = get_metadata(host, user, password, database2)
 
     # Database 3
     database3 = "world_x"
 
-    metadata3 = get_database_metadata(host, user, password, database3)
+    metadata3, relations3 = get_metadata(host, user, password, database3)
 
     combined_metadata = {**metadata1, **metadata2, **metadata3}
+    combined_relations = {**relations1, **relations2, **relations3}
 
     json_string_combined = convert_metadata_to_json(combined_metadata)
     print("Combined Metadata:")
     print(json_string_combined)
 
     similarity_threshold = 0.8
-    clusters_combined = find_person_columns(combined_metadata, similarity_threshold=similarity_threshold)
+    clusters_combined = find_person_columns(combined_metadata, combined_relations,
+                                            similarity_threshold=similarity_threshold)
     print(f"Found {len(clusters_combined)} similarities in the combined metadata:")
     for cluster_idx, representative_columns in clusters_combined.items():
         print(f"Cluster {cluster_idx}:")
         print(representative_columns)
         print("\n")
-
